f4953a99ce3ce0bd871e7374d79e3a144c8960a2
florian wagrez <florian.wagrez@gmail.com>
2024-03-06 16:57:19 +0100

    yelp is on rails dude

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,18 @@
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+#   en
+require 'faker'
+
+puts 'Creating 10 fake restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save!
+
+end
+puts 'Finished!'
